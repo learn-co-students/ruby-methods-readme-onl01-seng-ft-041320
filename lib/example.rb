d8d4aa10de8d9b1a48fb8c06975c876d8de038c7
Mercedes Dorsey <dorseymercedes@gmail.com>
2020-04-14 00:09:58 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,5 @@
 # Use this space to code along with the readme
-def say_hello_world_leaten_times
+def say_hello_world_ten_times
 phrase = "Hello World!"
 puts phrase
 puts phrase
@@ -12,3 +12,5 @@ puts phrase
 puts phrase
 puts phrase
 end
+
+say_hello_world_ten_times
