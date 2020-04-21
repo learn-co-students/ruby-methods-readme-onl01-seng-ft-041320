04a1da3e4f892ccf677d48dc4391509a1e71190f
placid-prototype-6143 <sdsj17@yahoo.com>
2020-04-21 16:38:36 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,16 @@
 # Use this space to code along with the readme
+def Say_Hello_ten_times
+    phrase = "Hello World!"
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
+end 
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+Say_Hello_ten_times
